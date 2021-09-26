8bd24e7d0c74e1643f6860b41916fc4f61624f29
Peter Giromagny <peter.giromagny@viacesi.fr>
2021-09-26 19:09:52 +0200

    Update _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,9 @@
-import '../styles/globals.scss'
-import type { AppProps } from 'next/app'
+import React from 'react';
+import 'styles/globals.scss';
+import type { AppProps } from 'next/app';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
-export default MyApp
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return <Component {...pageProps} />;
+};
+
+export default MyApp;
