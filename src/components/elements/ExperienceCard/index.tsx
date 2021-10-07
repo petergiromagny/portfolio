71da8fdba8dd590ad2c0d3f6dd97b38eba2fa54e
Peter Giromagny <peter.giromagny@viacesi.fr>
2021-10-07 10:15:13 +0200

    Create card element

---
+++ src/components/elements/ExperienceCard/index.tsx
@@ -0,0 +1,53 @@
+import React, { FC } from "react";
+import Image from "next/image";
+
+interface Props {
+  title: string;
+  location: string;
+  date: string;
+  text?: string | null;
+  url?: string | null;
+  urlName?: string;
+}
+
+const Index: FC<Props> = ({
+  title,
+  location,
+  date,
+  text,
+  url = null,
+  urlName = "web agency",
+}) => {
+  return (
+    <div className='experience__card'>
+      <div className='card__title'>
+        {url ? (
+          <a href={url} rel='nofollow noreferrer' target='_blank'>
+            <h3>{title}</h3>
+            {urlName && <span>{urlName}</span>}
+            <div className='arrow__image'>
+              <Image
+                src='/svg/arrow-link.svg'
+                alt='See website'
+                width={25}
+                height={25}
+              />
+            </div>
+          </a>
+        ) : (
+          <h3>{title}</h3>
+        )}
+      </div>
+      <div className='card__subtitle'>
+        <h4>
+          {location} <span>|</span> {date}
+        </h4>
+      </div>
+      <div className='card__text'>
+        <p>{text}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Index;
