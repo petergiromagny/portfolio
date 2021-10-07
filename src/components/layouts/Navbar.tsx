4e02897b25a2c1858254498afc892749c8c9dd71
Peter Giromagny <peter.giromagny@viacesi.fr>
2021-10-07 17:39:36 +0200

    Add navbar for footer

--- src/components/layouts/Navbar.tsx
+++ src/components/layouts/Navbar.tsx
@@ -1,18 +1,39 @@
-import React from 'react';
+import React, { FC } from "react";
 // Import components
-import Button from 'components/elements/Button';
+import Button from "components/elements/Button";
 
-const Index = () => {
+interface Props {
+  isFooter?: boolean;
+}
+
+const Index: FC<Props> = ({ isFooter = false }) => {
+  if (isFooter) {
+    return (
+      <div className='navbar__container navbar__container--footer container'>
+        <div className='navbar__list'>
+          <a href='#experience' className='navbar__link'>
+            About
+          </a>
+          <a href='#skill' className='navbar__link'>
+            Skills
+          </a>
+          <a href='#project' className='navbar__link'>
+            Projects
+          </a>
+        </div>
+      </div>
+    );
+  }
   return (
     <div className='navbar__container container'>
       <div className='navbar__list'>
-        <a href='#' className='navbar__link'>
+        <a href='#experience' className='navbar__link'>
           About
         </a>
-        <a href='#' className='navbar__link'>
+        <a href='#skill' className='navbar__link'>
           Skills
         </a>
-        <a href='#' className='navbar__link'>
+        <a href='#project' className='navbar__link'>
           Projects
         </a>
         <Button text='Contact me' />
