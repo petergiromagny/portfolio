bf66492c1c4efde2063a192a582c6f02ff172c9f
Peter Giromagny <peter.giromagny@viacesi.fr>
2021-10-07 17:39:15 +0200

    Create Footer

---
+++ src/components/layouts/Footer.tsx
@@ -0,0 +1,29 @@
+import React, { FC } from "react";
+// Import components
+import Button from "components/elements/Button";
+import Navbar from "./Navbar";
+
+const Index: FC = () => {
+  return (
+    <footer id='footer__content'>
+      <div className='container'>
+        <div className='section__header'>
+          <span className='emoji'>✉️</span>
+          <h2>Let&apos;s Work Together</h2>
+        </div>
+        <div className='footer__body'>
+          <p>
+            If you have a website or mobile app idea in mind, feel free to
+            contact me.
+          </p>
+          <a href='mailto:peter.giromagny@me.com' className='button'>
+            Say Hello!
+          </a>
+          <Navbar isFooter />
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Index;
