fed3996fcc2be1039717c94385f46423dc0838ce
Peter Giromagny <peter.giromagny@viacesi.fr>
2021-10-09 19:57:14 +0200

    Update app with global container

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,54 @@
-import React from 'react';
-import 'styles/globals.scss';
-import type { AppProps } from 'next/app';
+import React, { useEffect, useState } from "react";
+import "styles/globals.scss";
+import Image from "next/image";
+import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
+// Import layouts
+import Header from "components/layouts/Header";
+import Footer from "components/layouts/Footer";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  const [isVisible, setIsVisible] = useState<boolean>(false);
+
+  const router = useRouter();
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  useEffect(() => {
+    document.addEventListener("scroll", () => {
+      if (window.pageYOffset > 500) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    });
+  }, [router.pathname]);
+
+  return (
+    <div className='page__container'>
+      <Header />
+      <Component {...pageProps} />
+      <Footer />
+      <div
+        className={`scroll__content ${
+          isVisible ? "scroll__content--on" : "scroll__content--off"
+        }`}
+        onClick={scrollToTop}
+      >
+        <Image
+          src='/svg/arrowScroll.svg'
+          alt='Go to top'
+          width={60}
+          height={60}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default MyApp;
