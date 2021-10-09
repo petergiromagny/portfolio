21937fe02d38e01d3d8c4714160b2f5e784f2d0a
Peter Giromagny <peter.giromagny@viacesi.fr>
2021-10-09 19:56:35 +0200

    Create card component for project screen

--- src/components/elements/ProjectCard/index.tsx
+++ src/components/elements/ProjectCard/index.tsx
@@ -1,22 +1,70 @@
 import React, { FC } from "react";
 import Image from "next/image";
+import Link from "next/link";
 // Import components
-import Button from "../Button";
+import Tag from "../Tag";
 
 interface Props {
   id?: number;
   name?: string;
   intro?: string;
   image?: string;
+  tags?: string[];
   openProject?: boolean;
+  isHorizontal?: boolean;
 }
 
-const Index: FC<Props> = ({ id, name, intro, image, openProject = false }) => {
+const Index: FC<Props> = ({
+  id,
+  name,
+  intro,
+  image,
+  tags = [],
+  openProject = false,
+  isHorizontal = false,
+}) => {
+  // Project card to see more
   if (openProject) {
     return (
       <div className='project__card project__card--more'>
         <span>Don&apos;t keep the suspense alive</span>
-        <Button text='See more' />
+        <Link href='/project'>
+          <a className='button'>See more</a>
+        </Link>
+      </div>
+    );
+  }
+
+  // Card for project page
+  if (isHorizontal) {
+    return (
+      <div className='project__card project__card--horizontal'>
+        <div className='card__body'>
+          <div className='col__2 col__text'>
+            <h2>{name}</h2>
+            <div className='project__info'>
+              <p>{intro}</p>
+              <div className='project__tags'>
+                {tags.map((item, index) => {
+                  return <Tag text={item} key={index} />;
+                })}
+              </div>
+            </div>
+            <Link href='/project' scroll={true}>
+              <a className='button'>See more</a>
+            </Link>
+          </div>
+          <div className='col__2'>
+            <div className='preview'>
+              <Image
+                src={`/svg/previews/${image}`}
+                alt='Feu official'
+                width={400}
+                height={250}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
