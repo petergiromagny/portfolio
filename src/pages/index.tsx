c1926fd93e5fbf482cede21dc95650b7d94a08a8
Peter Giromagny <peter.giromagny@viacesi.fr>
2021-10-09 19:58:00 +0200

    Update Home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,7 @@
 import React, { useEffect, useState } from "react";
-import Image from "next/image";
+import dynamic from "next/dynamic";
 import type { NextPage } from "next";
 // Import layouts
-import Header from "components/layouts/Header";
 import Body from "components/layouts/Body";
 import Navbar from "components/layouts/Navbar";
 import Footer from "components/layouts/Footer";
@@ -13,49 +12,17 @@ import Skill from "components/sections/Skill";
 import Project from "components/sections/Project";
 
 const Home: NextPage = () => {
-  const [isVisible, setIsVisible] = useState<boolean>(false);
-
-  const scrollToTop = () => {
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth",
-    });
-  };
-
-  useEffect(() => {
-    document.addEventListener("scroll", () => {
-      if (window.pageYOffset > 500) {
-        setIsVisible(true);
-      } else {
-        setIsVisible(false);
-      }
-    });
-  }, []);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
   return (
-    <div className='page__container'>
-      <Header />
+    <div className='home__content'>
       <Body>
         <Navbar />
-        <Launch />
+        <Launch setIsLoading={setIsLoading} />
         <Experience />
         <Skill />
         <Project />
       </Body>
-      <Footer />
-      <div
-        className={`scroll__content ${
-          isVisible ? "scroll__content--on" : "scroll__content--off"
-        }`}
-        onClick={scrollToTop}
-      >
-        <Image
-          src='/svg/arrowScroll.svg'
-          alt='Go to top'
-          width={60}
-          height={60}
-        />
-      </div>
     </div>
   );
 };
