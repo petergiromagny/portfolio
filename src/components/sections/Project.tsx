ffcff0a16ac0c492ad70cbbf24bd6df16f393509
Peter Giromagny <peter.giromagny@viacesi.fr>
2021-10-11 09:18:54 +0200

    Update project section to display three last project on home page

--- src/components/sections/Project.tsx
+++ src/components/sections/Project.tsx
@@ -1,7 +1,15 @@
 import React, { FC } from "react";
 import ProjectCard from "components/elements/ProjectCard";
+// Import data
+import project from "data/project.json";
 
 const Index: FC = () => {
+  const lastProjects = [
+    project[project.length - 1],
+    project[project.length - 2],
+    project[project.length - 3],
+  ];
+
   return (
     <div id='project'>
       <div className='container'>
@@ -10,9 +18,16 @@ const Index: FC = () => {
           <h2>Recent Projects</h2>
         </div>
         <div className='project__body'>
-          <ProjectCard />
-          <ProjectCard />
-          <ProjectCard />
+          {lastProjects.map((item) => (
+            <ProjectCard
+              key={item.id}
+              id={item.id}
+              tags={item.tags}
+              name={item.name}
+              intro={item.intro}
+              image={item.image}
+            />
+          ))}
           <ProjectCard openProject />
         </div>
       </div>
